7d4d9233440fe61509e648d92609ceaafcff7c28
clenemt <clement.moron@gmail.com>
2020-05-26 13:13:15 -0400

    feat: add tracking with cookie

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,11 +6,21 @@ import 'lazysizes';
 $('[data-fancybox]').fancybox({ buttons: ['zoom', 'close'] });
 
 // Open mailto links in a new tab
-$("a[href^='mailto:']").click(function(e) {
+$("a[href^='mailto:']").click(function (e) {
   e.preventDefault();
   window.open($(this).attr('href'));
 });
 
 // Simple tracking
-const $form = $("[data-netlify]");
-if ($form.length) $.post($form.attr("action"), $form.serialize());
+const COOKIE = 'F_C_AGNES';
+const $form = $('[data-form]');
+if (
+  $form.length &&
+  !document.cookie.split(';').find((cookie) => cookie.startsWith(COOKIE))
+) {
+  $.post($form.attr('action'), $form.serialize()).then(() => {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + 10 * 365 * 24 * 60 * 60);
+    document.cookie = `${COOKIE}=true; expires=${expires.toUTCString()}; path=/`;
+})
+}
