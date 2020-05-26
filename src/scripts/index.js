e127bdacf839900f5e14c7f0cd0c3b419872dd42
clenemt <clement.moron@gmail.com>
2020-05-26 12:15:23 -0400

    feat: add contact link

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,3 +4,9 @@ import 'lazysizes';
 
 // Try to load all image as fancy box
 $('[data-fancybox]').fancybox({ buttons: ['zoom', 'close'] });
+
+// Open mailto links in a new tab
+$("a[href^='mailto:']").click(function(e) {
+  e.preventDefault();
+  window.open($(this).attr('href'));
+});
